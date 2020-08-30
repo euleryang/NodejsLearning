4b6fa95fec46ae09a4b10ca31b620a17ab40eb7f
euleryang <huan_yang2006@163.com>
2020-08-31 00:15:10 +0800

    atkFile.test

--- src/test/atkFileDeal.test.js
+++ src/test/atkFileDeal.test.js
@@ -6,19 +6,20 @@
  * @description
  *
  */
-var lib = require("../atkFileDeal");
+var libATK = require("../atkFileDeal");
+var lib = require("../index");
 var fs = require('fs');
 var muk = require("muk");
 var rewire = require("rewire");
+var assert = require("assert")
 
 describe('module ATKFileDeal', function () {
 
   // 普通测试
   describe('limit', function () {
     it('limit should success', function () {
-      lib.limit(10).should.be.equal(10);
-      lib.limit(-1).should.be.equal(0);
-      lib.limit(-2).should.be.equal(0);
+      assert.equal(lib.limit(10), 10);
+      assert.equal(lib.limit(-1), 0);
     });
   });
 
@@ -28,7 +29,7 @@ describe('module ATKFileDeal', function () {
       var lib = rewire('../index');
       var add = lib.__get__('_adding');
       var sum = add(1, 3);
-      sum.should.equal(4);
+      assert.equal(sum, 4);
     });
   });
 
--- src/test/index.test.js
+++ src/test/index.test.js
@@ -10,45 +10,58 @@ var lib = require("../index");
 var fs = require('fs');
 var muk = require("muk");
 var rewire = require("rewire");
+var assert = require("assert");
+
+describe('Array', function(){
+    describe('#indexOf()', function(){
+          it('should return -1 when the value is not present', function(){
+            assert.equal(-1, [1,2,3].indexOf(5));
+            assert.equal(-1, [1,2,3].indexOf(0));
+        })
+    })
+});
 
 describe('module', function () {
 
   // 普通测试
   describe('limit', function () {
     it('limit should success', function () {
-      lib.limit(10).should.be.equal(10);
-      lib.limit(-1).should.be.equal(0);
+      assert.equal(lib.limit(10), 10);
+      assert.equal(lib.limit(-1), 0);
+      //lib.limit(10).should.be.equal(10);
+      //lib.limit(-1).should.be.equal(0);
     });
   });
 
-//   // 异步测试
-//   describe('async', function () {
-//     it('async', function (done) {
-//       lib.async(function (result) {
-//         done();
-//       });
-//     });
-//   });
+  // 异步测试
+  describe('async', function () {
+    it('async', function (done) {
+      lib.async(function (result) {
+        done();
+      });
+    });
+  });
 
-//   // 异常测试
-//   describe("getContent", function () {
-//     before(function () {
-//       muk(fs, 'readFile', function(path, encoding, callback) {
-//         process.nextTick(function () {
-//           callback(new Error("mock readFile error"));
-//         });
-//       });
-//     });
-//     it('getContent', function (done) {
-//       lib.getContent('text.txt', function (err, file) {
-//         err.should.be.ok();
-//         done();
-//       })
-//     });
-//     after(function () {
-//       muk.restore();
-//     });
-//   });
+  // 异常测试
+  describe("getContent", function () {
+    before(function () {
+      muk(fs, 'readFile', function(path, encoding, callback) {
+        process.nextTick(function () {
+          callback(new Error("mock readFile error"));
+        });
+      });
+    });
+    it('getContent', function (done) {
+      lib.getContent('text.txt', function (err, file) {
+        assert.ok(err);
+        //err.should.be.ok();
+        done();
+      })
+    });
+    after(function () {
+      muk.restore();
+    });
+  });
 
   // 测试私有方法
   describe('add', function () {
@@ -56,7 +69,8 @@ describe('module', function () {
       var lib = rewire('../index');
       var add = lib.__get__('_adding');
       var sum = add(1, 3);
-      sum.should.equal(4);
+      assert.equal(sum, 4);
+      //sum.should.equal(4);
     });
   });
 });

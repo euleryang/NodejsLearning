ade23136488cd3361bdb1d8ce32adb4d4c1a3ba4
euleryang <huan_yang2006@163.com>
2020-08-30 23:16:04 +0800

    index.test.js

--- src/index.js
+++ src/index.js
@@ -26,9 +26,21 @@ console.log("Hello World!")
 
 // })
 
+
 exports.limit = function (num) {
-    if (num < 0) {
-      return 0;
-    }
-    return num;
-  };
+  return num < 0 ? 0 : num;
+};
+
+exports.async = function (callback) {
+  setTimeout(function () {
+    callback(10);
+  }, 10);
+};
+
+exports.getContent = function (filename, callback) {
+  fs.readFile(filename, 'utf-8', callback);
+};
+
+function _adding(num1, num2) {
+  return num1 + num2;
+}

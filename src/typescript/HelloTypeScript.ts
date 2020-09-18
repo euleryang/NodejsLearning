984c660a629e1abef45ed4c0d57d6177fddc9a86
euleryang <huan_yang2006@163.com>
2020-09-19 02:03:36 +0800

    typescript lite-server demo

---
+++ src/typescript/HelloTypeScript.ts
@@ -0,0 +1,15 @@
+class HelloTypeScript {
+    helloString: string;
+    constructor(message: string) {
+        this.helloString = message;
+    }
+    hello() {
+        return this.helloString;
+    }
+  }
+
+  let myName: string = 'SanShui'
+  let myAge: number = 23
+  let sentence: string = `Hello, my name is ${myName}. I'll be ${myAge + 1} years old next month`
+  let helloTypeScript = new HelloTypeScript(sentence);
+  document.body.innerHTML = helloTypeScript.hello();

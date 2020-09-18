cd0e742073d308bcd449de84a8a5c8ac19c9c8c9
euleryang <huan_yang2006@163.com>
2020-09-19 00:29:13 +0800

    typescript ruan

--- src/tsserver/index.js
+++
@@ -1,9 +0,0 @@
-// server.ts
-var express = require("express");
-var app = express();
-app.get('/', function (req, res) {
-    res.send('hello world!');
-});
-var server = app.listen(3001, function () {
-    console.log('Listen on port 3001');
-});
